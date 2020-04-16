66817d7d2cd3c56a84dc0879244c8eb4dfe45312
gowthamaspi <gowtham@aspigrow.com>
2020-04-16 11:26:29 +0530

    Added content length

--- controller.py
+++ controller.py
@@ -76,6 +76,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 path = str(os.getcwd())+'/Document'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 file_path = ''
 document_data = ''
 document_data_initial = ''
